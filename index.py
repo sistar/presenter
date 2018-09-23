7cd66978068875eaaede877da90698ae5d2caa08
sistar <ralf.sigmund@gmail.com>
2018-09-23 15:27:31 +0200

    Add iot client

--- index.py
+++ index.py
@@ -5,10 +5,13 @@ iot_client = boto3.client('iot')
 iot_data_client = boto3.client('iot-data')
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
+    thing_name = 'esp8266_D4E3A2'
+    iot_client = boto3.client('iot')
+    iot_data_client = boto3.client('iot-data')
+
+    response = iot_client.describe_thing(thingName=thing_name)
+
+    data = iot_data_client.get_thing_shadow(thingName=thing_name)
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
