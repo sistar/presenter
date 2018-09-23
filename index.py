d8cdba5755ad9b1b3aea48ce30332970eebb23bf
sistar <ralf.sigmund@gmail.com>
2018-09-23 19:53:43 +0200

    query things

--- index.py
+++ index.py
@@ -5,17 +5,16 @@ iot_client = boto3.client('iot')
 iot_data_client = boto3.client('iot-data')
 
 def handler(event, context):
-    thing_name = 'esp8266_D4E3A2'
-    iot_client = boto3.client('iot')
-    iot_data_client = boto3.client('iot-data')
-
-    # response = iot_client.describe_thing(thingName=thing_name)
-
-    response = iot_data_client.get_thing_shadow(thingName=thing_name)
-    streamingBody = response["payload"]
-    rawDataBytes = streamingBody.read()  # rawDataBytes is of type 'bytes' in, Python 3.x specific
-    rawDataString = rawDataBytes.decode('utf-8')  # Python 3.x specific
-    jsonState = json.loads(rawDataString)
+    things = iot_client.list_things()
+    summary = []
+    for thing_name in things:
+        # response = iot_client.describe_thing(thingName=thing_name)
+        response = iot_data_client.get_thing_shadow(thingName=thing_name)
+        streamingBody = response["payload"]
+        rawDataBytes = streamingBody.read()  # rawDataBytes is of type 'bytes' in, Python 3.x specific
+        rawDataString = rawDataBytes.decode('utf-8')  # Python 3.x specific
+        jsonState = json.loads(rawDataString)
+        summary.append(jsonState)
     return {'statusCode': 200,
-            'body': json.dumps(jsonState),
+            'body': json.dumps(summary),
             'headers': {'Content-Type': 'application/json'}}
