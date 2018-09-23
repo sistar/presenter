5ed6992d4ca4c8560829bdb2f48ccfac81a7235b
sistar <ralf.sigmund@gmail.com>
2018-09-23 19:53:43 +0200

    query things

--- index.py
+++ index.py
@@ -7,9 +7,9 @@ iot_data_client = boto3.client('iot-data')
 def handler(event, context):
     things = iot_client.list_things()
     summary = []
-    for thing_name in things:
+    for thing in things:
         # response = iot_client.describe_thing(thingName=thing_name)
-        response = iot_data_client.get_thing_shadow(thingName=thing_name)
+        response = iot_data_client.get_thing_shadow(thingName=thing["thingName"])
         streamingBody = response["payload"]
         rawDataBytes = streamingBody.read()  # rawDataBytes is of type 'bytes' in, Python 3.x specific
         rawDataString = rawDataBytes.decode('utf-8')  # Python 3.x specific
